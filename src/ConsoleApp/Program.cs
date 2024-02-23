650cb5334cf71283f294cfa6abf980b9ef353410
Mario Ramón <marioru699@gmail.com>
2024-02-23 13:24:11 -0600

    Proyecto Bimestrar parte 2.Triangulo.

---
+++ src/ConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+double ladoA = 10;
+double ladoC = 10;
+double ladoB = 20;
+
+double altura = 15;
+
+
+double perimetro = Application.Triangulo.perimetro(ladoA,ladoB,ladoC);
+double area = Application.Triangulo.area(ladoB,altura);
+double volumen = Application.Triangulo.volumen(area,altura);
+
+
+Console.WriteLine($"El perímetro de un triangulo de lado A {ladoA}cm, lado B Base {ladoB}cm y lado C {ladoC}cm es de {perimetro}cm");
+Console.WriteLine($"El área de un triangulo con base = {ladoB}cm y altura h = {altura}cm es de  {area}cm. ");
+Console.WriteLine($"El volumen de un triangulo es de  {volumen}cm. ");
